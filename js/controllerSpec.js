87fc3c23e5d935ee9099021fce37bd6cda7118ff
Kin <winterheat@gmail.com>
2016-03-12 18:48:55 -0800

    ok

---
+++ js/controllerSpec.js
@@ -0,0 +1,18 @@
+
+
+describe("Controller: ListCtrl", function() {
+    beforeEach(module("myApp"));
+
+    var ctrl;
+
+    beforeEach(inject(function($controller) {
+        ctrl = $controller("ListCtrl");
+    }));
+
+    it("should have items available on load", function() {
+        expect(ctrl.items).toEqual([
+            {id: 1, label: "First", done: true },
+            {id: 2, label: "Second", done: false }        
+        ]);
+    });
+});
