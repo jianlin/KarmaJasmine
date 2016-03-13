7648cb1756d8b27d6358992b904d2c3a929b8f05
Kin <winterheat@gmail.com>
2016-03-12 18:36:06 -0800

    ok

---
+++ js/controller.js
@@ -0,0 +1,18 @@
+
+
+angular.module("myApp", [])
+  .controller("ListCtrl", function() {
+      var vm = this;
+
+      vm.items = [
+          {id: 1, label: "First", done: true },
+          {id: 2, label: "Second", done: false }
+      ];
+
+      vm.getDoneClass = function(item) {
+          return {
+              finished: item.done,
+              unfinished: !item.done
+          };
+      }
+  });
